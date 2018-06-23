777290a7ae2818a16d0230b8c7f1427fe5d0d3d3
Valeria Ragonese <ribesnero80@gmail.com>
2018-06-23 15:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma()
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+  return "I love you too"
+}
